e8643a1549054dad2bd72320f63d0bba61d0813e
Mauricio Cruz <cruzhernandezmauricio4@gmail.com>
2026-09-23 03:23:53 -0600

    realizacion de node.js

---
+++ mi-exploracion-espacial/index.js
@@ -0,0 +1,10 @@
+// index.js
+// Importamos el array de planetas
+const planetas = require('./planetas');
+
+planetas.forEach(planeta => {
+  console.log(`¡Planeta ${planeta.nombre} descubierto!`);
+  console.log(`Descripción: ${planeta.descripcion}`);
+  console.log(`Descubierto en: ${planeta.descubiertoEn}`);
+  console.log('---');
+});
---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,18 @@
+// planetas.js
+// Array de planetas favoritos con propiedades básicas
+// planetas.js
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  }
+];
+
+// Exportamos el array
+module.exports = planetas;
